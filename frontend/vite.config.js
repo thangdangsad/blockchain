bb723a59ede2bbeb1e69ca3f3737177efe264471
thang <duongthang.1903@gmail.com>
2025-11-27 14:00:33 +0700

    a

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        host: '0.0.0.0', // cho phép mọi IP truy cập
+        port: 5173,      // port muốn mở
+    },
 })
