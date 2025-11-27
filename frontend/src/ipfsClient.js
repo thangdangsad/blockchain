f7438ed63c72981aaacc94409f637a3bb102f32d
thang <duongthang.1903@gmail.com>
2025-11-27 15:00:03 +0700

    a

--- frontend/src/ipfsClient.js
+++ frontend/src/ipfsClient.js
@@ -2,6 +2,6 @@
 import { create } from "ipfs-http-client";
 
 // Kết nối đến node IPFS Desktop local
-const ipfsClient = create({ url: "http://127.0.0.1:5001" });
+const ipfsClient = create({ url: "http://160.191.175.191:5001" });
 
 export default ipfsClient;
